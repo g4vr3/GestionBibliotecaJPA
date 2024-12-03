43d92f083b52b423a0eac33f3b167fbb2ebd3d53
g4vr3 <gdfizan@gmail.com>
2024-12-03 09:29:04 +0100

    Add repository/data access layer

---
+++ src/main/java/repositorio/GenericDAO.java
@@ -0,0 +1,47 @@
+package repositorio;
+
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class GenericDAO<T> {
+    private Class<T> classType;
+    private EntityManagerFactory emf = Persistence.createEntityManagerFactory("equipos");
+    private EntityManager em = emf.createEntityManager();
+    private EntityTransaction et = em.getTransaction();
+
+    public GenericDAO(Class<T> classType) {
+        this.classType = classType;
+    }
+
+    public void create(T entity) {
+        et.begin();
+        em.persist(entity);
+        et.commit();
+    }
+
+    public T read (Integer id) {
+        return em.find(classType, id);
+    }
+
+    public List<T> readAll() {
+        return em.createQuery("SELECT e FROM " + classType.getSimpleName() + " e", classType).getResultList();
+    }
+
+    public void update (T entity) {
+        et.begin();
+        em.merge(entity);
+        et.commit();
+    }
+
+    public void delete (T entity) {
+        et.begin();
+        em.remove(entity);
+        et.commit();
+    }
+}
+
