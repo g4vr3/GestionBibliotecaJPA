f87c9b368601cb07a2cdb9da4a5cf303fbb3bb17
g4vr3 <gdfizan@gmail.com>
2024-12-03 09:26:38 +0100

    Add model layer

---
+++ src/main/java/modelo/Ejemplar.java
@@ -0,0 +1,63 @@
+package modelo;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+public class Ejemplar {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "isbn", nullable = false)
+    private modelo.Libro isbn;
+
+    @ColumnDefault("'Disponible'")
+    @Lob
+    @Column(name = "estado")
+    private String estado;
+
+    @OneToMany(mappedBy = "ejemplar")
+    private Set<modelo.Prestamo> prestamos = new LinkedHashSet<>();
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public modelo.Libro getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(modelo.Libro isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public Set<modelo.Prestamo> getPrestamos() {
+        return prestamos;
+    }
+
+    public void setPrestamos(Set<modelo.Prestamo> prestamos) {
+        this.prestamos = prestamos;
+    }
+
+}
---
+++ src/main/java/modelo/Libro.java
@@ -0,0 +1,43 @@
+package modelo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Libro {
+    @Id
+    @Column(name = "isbn", nullable = false, length = 20)
+    private String isbn;
+
+    @Column(name = "titulo", nullable = false, length = 200)
+    private String titulo;
+
+    @Column(name = "autor", nullable = false, length = 100)
+    private String autor;
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+}
